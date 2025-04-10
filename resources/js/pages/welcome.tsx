e78f953f7f0abe5f02286e2f26f8b41c27004e8e
DanielHemmati <danialups1997@gmail.com>
2025-04-10 15:45:13 +0200

    start working on demo

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -10,8 +10,8 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
+            <div className="container mx-auto bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a]">
+                <header className="mb-6 w-full text-sm not-has-[nav]:hidden">
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? (
                             <Link
@@ -38,8 +38,8 @@ export default function Welcome() {
                         )}
                     </nav>
                 </header>
-                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
+                <div className="debug w-full lg:grow starting:opacity-0">
+                    <main className="">
                         <h1>hello world</h1>
                     </main>
                 </div>
