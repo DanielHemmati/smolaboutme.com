286990e6ba053f233a98d0e16dd53d4bf57d5da7
DanielHemmati <danialups1997@gmail.com>
2025-04-13 21:02:43 +0200

    Redirect user to /u/<username>/editor on login and register

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        // return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('user.editor', ['username' => $request->user()->name], absolute: false));
     }
 
     /**
