0e3d09a3b1276c8f4b98d08b2b2e225b3ba7bfae
DanielHemmati <danialups1997@gmail.com>
2025-04-14 21:34:56 +0200

    move example content from tiptap

---
+++ resources/js/lib/exmapleContent.ts
@@ -0,0 +1,30 @@
+// for showing example content in the editor
+const ExampleContent = `
+<h2>
+  Hello wip,
+</h2>
+<p>
+  this is a <em>basic</em> example of <strong>smolaboutme.com</strong>. I am using tiptap to create a simple text editor. Some stuff are paid i have to build them myself
+</p>
+<ul>
+  <li>
+    That's a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  Isn't that great? And all of that is editable. But wait, there's more. Let's try a code block:
+</p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+<blockquote>
+  You once told me that the human eye is God's loneliest creation. How so much of the world passes through the pupil and still it holds nothing.
+  <br />
+  ~ Ocean Vuong, On Earth We're Briefly Gorgeous
+</blockquote>
+`;
+
+export default ExampleContent;
