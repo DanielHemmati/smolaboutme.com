39a704f175ef9a59b0bcd58d28c5e2fe06734e8f
DanielHemmati <danialups1997@gmail.com>
2025-04-21 16:58:52 +0200

    wip

---
+++ resources/js/pages/test.tsx
@@ -0,0 +1,7 @@
+export default function Test() {
+    return (
+        <div className="mx-auto max-w-4xl h-screen flex items-center justify-center">
+            <h1>hello world</h1>
+        </div>
+    );
+}
